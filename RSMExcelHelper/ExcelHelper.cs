0e9bdbd3f85917d6a6394baabe6fdc3a112fa108
nikaku <qurdadzenika@gmail.com>
2020-03-11 12:29:21 +0400

    Fixed Background Process Bug And UpdateNuget Version

--- RSMExcelHelper/ExcelHelper.cs
+++ RSMExcelHelper/ExcelHelper.cs
@@ -1,39 +1,55 @@
 ﻿using System;
 using System.IO;
+using System.Runtime.InteropServices;
 using Excel = Microsoft.Office.Interop.Excel;
 
 namespace RSMExcelHelper
 {
     public class Helper
     {
-        public void GetAndSave(string sourceLocationPath, string targetLocationPath)
+        public void GenerateReadOnlyFile(string sourceLocationPath, string targetLocationPath)
         {
             DateTime now = DateTime.Now;
             Excel.Application xlApp = new Excel.Application { DisplayAlerts = false };
-            Excel.Workbook xlWorkBook = xlApp.Workbooks.Open(sourceLocationPath);
+            Excel.Workbooks xlWorkbooks = xlApp.Workbooks;
+            Excel.Workbook xlWorkBook = xlWorkbooks.Open(sourceLocationPath);
+
             string fileName = $"{targetLocationPath}-{now.Day - 1}-{now.Month}-{now.Year}.xlsx";
             if (File.Exists(fileName))
             {
-                Excel.Workbook xlWorkBook2 = xlApp.Workbooks.Open(fileName);
+                Excel.Workbook xlWorkBook2 = xlWorkbooks.Open(fileName);
                 if (xlWorkBook2.ReadOnly)
                 {
-                    xlWorkBook2.Close(true);
+                    xlApp.Quit();
+                    Marshal.ReleaseComObject(xlWorkBook);
+                    Marshal.ReleaseComObject(xlWorkbooks);
+                    Marshal.ReleaseComObject(xlApp);
                     return;
                 }
             }
             xlWorkBook.SaveAs(fileName);
-            xlWorkBook.Close(true);
+            xlWorkBook.Close();
             File.SetAttributes(fileName, FileAttributes.ReadOnly);
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlWorkBook);
+            Marshal.ReleaseComObject(xlWorkbooks);
+            Marshal.ReleaseComObject(xlApp);
         }
         public void RefreshFile(string sourceLocationPath)
         {
             Excel.Application xlApp = new Excel.Application { DisplayAlerts = false };
-            Excel.Workbook xlWorkBook = xlApp.Workbooks.Open(sourceLocationPath);
+            Excel.Workbooks xlWorkbooks = xlApp.Workbooks;
+            Excel.Workbook xlWorkBook = xlWorkbooks.Open(sourceLocationPath);
             xlWorkBook.RefreshAll();
             xlApp.Application.CalculateUntilAsyncQueriesDone();
             xlWorkBook.Save();
             xlWorkBook.Close();
+            xlWorkbooks.Close();
             xlApp.Quit();
+            Marshal.ReleaseComObject(xlWorkBook);
+            Marshal.ReleaseComObject(xlWorkbooks);
+            Marshal.ReleaseComObject(xlApp);
+
         }
     }
 }
